bbcd63bea80779f2a6af8f71136049ae3c7e603c
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-10 15:31:18 +0100

    Add tests for problem 2.

---
+++ src/test/java/com/github/pedrovgs/app/problem2/VectorScalarProductTest.java
@@ -0,0 +1,53 @@
+package com.github.pedrovgs.app.problem2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class VectorScalarProductTest {
+
+  private VectorScalarProduct vectorScalarProduct;
+
+  @Before public void setUp() {
+    vectorScalarProduct = new VectorScalarProduct();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void differentSizeVectorsShouldThrowException() {
+    Vector v1 = new Vector(1, 2, 3, 4);
+    Vector v2 = new Vector(1, 2);
+
+    vectorScalarProduct.calculateScalarProduct(v1, v2);
+  }
+
+  @Test public void shouldReturnZeroIfOneVectorIsFullOfZeros() {
+    Vector v1 = new Vector(1, 2, 3, 4);
+    Vector v2 = new Vector(0, 0, 0, 0);
+
+    int result = vectorScalarProduct.calculateScalarProduct(v1, v2);
+
+    assertEquals(0, result);
+  }
+
+  @Test public void shouldReturnSumOfOneVectorIfOtherIsFullOfOnes() {
+    Vector v1 = new Vector(2, 2, 2, 2);
+    Vector v2 = new Vector(1, 1, 1, 1);
+
+    int result = vectorScalarProduct.calculateScalarProduct(v1, v2);
+
+    assertEquals(8, result);
+  }
+
+  @Test public void shouldTakeIntoAccountNegativeElements() {
+    Vector v1 = new Vector(2, 2, 2, 2);
+    Vector v2 = new Vector(-1, 1, -1, -1);
+
+    int result = vectorScalarProduct.calculateScalarProduct(v1, v2);
+
+    assertEquals(-2, result);
+  }
+}
