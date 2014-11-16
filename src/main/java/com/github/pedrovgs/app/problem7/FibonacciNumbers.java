f5b9b2b9f47f37015ec8d9ac82b6457dd0c9c5cc
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-16 15:23:11 +0100

    Add statement for problem 7 and some tests.

---
+++ src/main/java/com/github/pedrovgs/app/problem7/FibonacciNumbers.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.app.problem7;
+
+/**
+ * In mathematics, the Fibonacci numbers or Fibonacci sequence are the numbers in the following
+ * integer sequence:
+ *
+ * 0,1,1,2,3,5,8,13,21,34.
+ *
+ * The Fibonacci spiral: an approximation of the golden spiral created by drawing circular arcs
+ * connecting the opposite corners of squares in the Fibonacci tiling;this one uses squares of
+ * sizes 1, 1, 2, 3, 5, 8, 13, 21, and 34.
+ * By definition, the first two numbers in the Fibonacci sequence are 1 and 1, or 0 and 1,
+ * depending on the chosen starting point of the sequence, and each subsequent number is the sum of
+ * the previous two.
+ *
+ * Can you write a method to calculate the Nth Fibonacci number?
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class FibonacciNumbers {
+  public int get(int n) {
+    return -1;
+  }
+}
