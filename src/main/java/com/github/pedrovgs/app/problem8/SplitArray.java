09762f1873be185ad4695e47a59020524eb5e555
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-17 20:51:51 +0100

    Add statement for problem 8

---
+++ src/main/java/com/github/pedrovgs/app/problem8/SplitArray.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.app.problem8;
+
+/**
+ * Given an array full of integers positive or negative write a method to move every negative
+ * number to the left and every positive number to the right. Take into account that the order of
+ * this elements into the array doesn't care.
+ *
+ * Input: [1,2,3,-1-,2,-3]
+ * Output: [-2,-1,-3,2,3,1]
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class SplitArray {
+  
+}
