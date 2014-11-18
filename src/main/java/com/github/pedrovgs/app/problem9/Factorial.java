a9f93af5dcd5017dbe12f383ec7c5526e34f5edb
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-18 21:11:27 +0100

    Add statement for problem 9

---
+++ src/main/java/com/github/pedrovgs/app/problem9/Factorial.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.app.problem9;
+
+/**
+ * In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all
+ * positive integers less than or equal to n. For example:
+ *
+ * 5! = 5  \times  4  \times  3  \times  2  \times  1 = 120.
+ *
+ * The value of 0! is 1, according to the convention for an empty product.
+ *
+ * Can you write a method to calculate the factorial of a given positive integer?
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class Factorial {
+
+}
