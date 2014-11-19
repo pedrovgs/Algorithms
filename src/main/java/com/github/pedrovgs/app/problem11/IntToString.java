238d25cd2bae6d3a52307a11389af3d5ebba234b
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-20 00:00:26 +0100

    Add statement for problem 11

---
+++ src/main/java/com/github/pedrovgs/app/problem11/IntToString.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.app.problem11;
+
+/**
+ * Given a integer, can you write a method to transform a given integer to String? Take into
+ * account
+ * you can receive negative integers.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class IntToString {
+}
