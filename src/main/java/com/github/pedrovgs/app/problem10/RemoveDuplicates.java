278316d926e3225692174954b9af3babdd102a3f
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-19 21:51:31 +0100

    Add statement for problem 10

---
+++ src/main/java/com/github/pedrovgs/app/problem10/RemoveDuplicates.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.app.problem10;
+
+/**
+ * Given an array full of integers, can you write a method returning other array without duplicated
+ * elements? The elements order doesn't care. For example:
+ *
+ * Input: [1,2,3,1] Output: [1,1,12]
+ * Input: [1,1,1,1,1] Output: [1]
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class RemoveDuplicates {
+}
