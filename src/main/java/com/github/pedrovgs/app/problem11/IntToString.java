511761a0f9633a443663029da9ffb208d5b52fbe
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-20 00:15:47 +0100

    Add implementation to problem 11

--- src/main/java/com/github/pedrovgs/app/problem11/IntToString.java
+++ src/main/java/com/github/pedrovgs/app/problem11/IntToString.java
@@ -24,8 +24,31 @@ package com.github.pedrovgs.app.problem11;
  */
 public class IntToString {
 
+  /**
+   * Algorithm implementation based on the module of a given number and iterate until the number is
+   * zero. With this approach we get a complexity order equals to O(N) in time terms where N is the
+   * number of digits in the given integer. In space terms, the complexity order is also O(N)
+   * because we are using an StringBuilder as auxiliary data structure.
+   */
   public String transform(int number) {
-    return null;
-  }
+    boolean isNegative = false;
+    StringBuilder sb = new StringBuilder();
+    if (number == 0) {
+      return "0";
+    } else if (number < 0) {
+      isNegative = true;
+    }
+    number = Math.abs(number);
 
+    while (number > 0) {
+      sb.append(number % 10);
+      number /= 10;
+    }
+    
+    if (isNegative) {
+      sb.append("-");
+    }
+
+    return sb.reverse().toString();
+  }
 }
