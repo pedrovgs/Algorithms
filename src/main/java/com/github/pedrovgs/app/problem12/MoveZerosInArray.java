13307d3be9ea71f3fa17c3d352b502bf365264c4
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-21 00:02:00 +0100

    Add problem 12

---
+++ src/main/java/com/github/pedrovgs/app/problem12/MoveZerosInArray.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.app.problem12;
+
+/**
+ * Given an array full of integers, can you write a method to move every zero to the right side of
+ * the array? The result order of non zero element is not important. Take into account you can find
+ * negative numbers inside the array. For example:
+ *
+ * Input: [1,2,0,4,3] Output: [1,2,3,4,0]
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class MoveZerosInArray {
+}
