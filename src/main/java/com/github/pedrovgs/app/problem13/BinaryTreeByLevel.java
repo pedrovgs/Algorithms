37c22f4b6d3318324f3589ec879a8dcb70c8d947
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-23 19:02:38 +0100

    Add second implementation to get a list of nodes by level without any additional data structure.

--- src/main/java/com/github/pedrovgs/app/problem13/BinaryTreeByLevel.java
+++ src/main/java/com/github/pedrovgs/app/problem13/BinaryTreeByLevel.java
@@ -16,6 +16,7 @@
 package com.github.pedrovgs.app.problem13;
 
 import com.github.pedrovgs.app.binarytree.BinaryNode;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -51,5 +52,57 @@ public class BinaryTreeByLevel {
       if (binaryNode.getRight() != null) queue.add(binaryNode.getRight());
     }
     return result;
+  }
+
+  /**
+   * Slower implementation created to resolve one of the questions of problem 13, print the binary
+   * tree by level without use any additional data structure for the main algorithm.
+   * The complexity order in space terms is O(1) because we are not using any other data structure.
+   * The complexity order in time terms is O(N*M) where the N is the depth of the Binary Tree and M
+   * is the number of elements below one level.
+   *
+   * To be able to implement this approach you need to know the depth of the tree before to start.
+   * This approach is based on recursion and uses the level param as counter to go through the
+   * binary tree.
+   */
+  public List<BinaryNode> getWihtoutAdditionalDataStructures(BinaryNode root) {
+    if (root == null) {
+      throw new IllegalArgumentException("You can't use null BinaryNodes as argument.");
+    }
+
+    List<BinaryNode> result = new LinkedList<BinaryNode>();
+    int depth = getDepth(root);
+    for (int i = 1; i <= depth; i++) {
+      result.addAll(getNodesForLevel(root, i));
+    }
+    return result;
+  }
+
+  /**
+   * Calculate the Binary Tree depth based on recursion. The complexity order in space terms of
+   * this algorithm is O(1) and in time terms is O(N) where N is the number of nodes in the tree.
+   */
+  private int getDepth(BinaryNode root) {
+    if (root == null) {
+      return 0;
+    } else {
+      return 1 + Math.max(getDepth(root.getLeft()), getDepth(root.getRight()));
+    }
+  }
+
+  private List<BinaryNode> getNodesForLevel(BinaryNode root, int level) {
+    if (root == null) {
+      return Collections.EMPTY_LIST;
+    } else {
+      List<BinaryNode> result = new LinkedList<BinaryNode>();
+      StringBuilder stringBuilder = new StringBuilder();
+      if (level == 1) {
+        result.add(root);
+      } else {
+        result.addAll(getNodesForLevel(root.getLeft(), level - 1));
+        result.addAll(getNodesForLevel(root.getRight(), level - 1));
+      }
+      return result;
+    }
   }
 }
--- src/test/java/com/github/pedrovgs/app/problem13/BinaryTreeByLevelTest.java
+++ src/test/java/com/github/pedrovgs/app/problem13/BinaryTreeByLevelTest.java
@@ -79,4 +79,51 @@ public class BinaryTreeByLevelTest {
     List<BinaryNode<Integer>> expectedList = Arrays.asList(root, n1, n2, n3, n4, n5);
     assertEquals(expectedList, result);
   }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldNotAcceptNullBinaryNodesWithoutExtraDataStructure() {
+    binaryTreeByLevel.getWihtoutAdditionalDataStructures(null);
+  }
+
+  @Test
+  public void shouldReturnTheSameNodeInTheListIfTheTreeJustContainsOneElementWithoutExtraDataStructure() {
+    BinaryNode<Integer> root = new BinaryNode<Integer>(0);
+
+    List<BinaryNode> result = binaryTreeByLevel.getWihtoutAdditionalDataStructures(root);
+
+    List<BinaryNode<Integer>> expectedList = Arrays.asList(root);
+    assertEquals(expectedList, result);
+  }
+
+  @Test public void shouldWorkWithALittleBinaryTreeWithoutExtraDataStructure() {
+    BinaryNode<Integer> root = new BinaryNode<Integer>(0);
+    BinaryNode<Integer> n1 = new BinaryNode<Integer>(1);
+    BinaryNode<Integer> n2 = new BinaryNode<Integer>(2);
+    root.setLeft(n1);
+    root.setRight(n2);
+
+    List<BinaryNode> result = binaryTreeByLevel.getWihtoutAdditionalDataStructures(root);
+
+    List<BinaryNode<Integer>> expectedList = Arrays.asList(root, n1, n2);
+    assertEquals(expectedList, result);
+  }
+
+  @Test public void shouldWorkWithABiggerBinaryTreeWithoutExtraDataStructure() {
+    BinaryNode<Integer> root = new BinaryNode<Integer>(0);
+    BinaryNode<Integer> n1 = new BinaryNode<Integer>(1);
+    BinaryNode<Integer> n2 = new BinaryNode<Integer>(2);
+    BinaryNode<Integer> n3 = new BinaryNode<Integer>(3);
+    BinaryNode<Integer> n4 = new BinaryNode<Integer>(4);
+    BinaryNode<Integer> n5 = new BinaryNode<Integer>(5);
+    root.setLeft(n1);
+    root.setRight(n2);
+    n1.setLeft(n3);
+    n1.setRight(n4);
+    n2.setLeft(n5);
+
+    List<BinaryNode> result = binaryTreeByLevel.getWihtoutAdditionalDataStructures(root);
+
+    List<BinaryNode<Integer>> expectedList = Arrays.asList(root, n1, n2, n3, n4, n5);
+    assertEquals(expectedList, result);
+  }
 }
