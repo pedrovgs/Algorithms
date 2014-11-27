146c71ea3299842ccd390c7c1a3beac4f47be291
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-28 00:29:25 +0100

    Add iterative implementation for problem 17

--- src/main/java/com/github/pedrovgs/app/problem17/BinaryTreeEquals.java
+++ src/main/java/com/github/pedrovgs/app/problem17/BinaryTreeEquals.java
@@ -16,6 +16,7 @@
 package com.github.pedrovgs.app.problem17;
 
 import com.github.pedrovgs.app.binarytree.BinaryNode;
+import java.util.Stack;
 
 /**
  * Given two binary trees, can you write a method to check if this trees are equals?
@@ -31,7 +32,8 @@ public class BinaryTreeEquals {
    * information.
    *
    * Related with the execution time of this algorithm is really important take into account the
-   * third recursion case where we are going to use "if" short circuit to avoid go through the whole
+   * third recursion case where we are going to use "if" short circuit to avoid go through the
+   * whole
    * tree and stop comparing nodes once we find one different.
    */
   public boolean areEqualsRecursive(BinaryNode tree1, BinaryNode tree2) {
@@ -50,6 +52,44 @@ public class BinaryTreeEquals {
       return tree1.equals(tree2)
           && areEqualsInner(tree1.getLeft(), tree2.getLeft())
           && areEqualsInner(tree1.getRight(), tree2.getRight());
+    }
+  }
+
+  /**
+   * Iterative solution for this algorithm. Using a tree traversal based on two parallel data
+   * structures implemented using one Stack. The complexity order of this algorithm is O(N) in time
+   * terms, as the previous one. But in space terms, the complexity order of this algorithm is O(N)
+   * where N is the number of elements in the smallest tree because we are going to store this
+   * elements in the stack.
+   */
+  public boolean areEqualsIterative(BinaryNode<Integer> tree1, BinaryNode<Integer> tree2) {
+    if (tree1 == null && tree2 == null) {
+      throw new IllegalArgumentException("You can't compare two null trees");
+    }
+
+    boolean equals = true;
+    Stack<BinaryNode> stack1 = new Stack<BinaryNode>();
+    Stack<BinaryNode> stack2 = new Stack<BinaryNode>();
+    stack1.push(tree1);
+    stack2.push(tree2);
+    while (!stack1.isEmpty()) {
+      BinaryNode node1 = stack1.pop();
+      BinaryNode node2 = stack2.pop();
+      if (!node1.equals(node2)) {
+        equals = false;
+        break;
+      }
+      addNodeToStack(stack1, node1.getLeft());
+      addNodeToStack(stack1, node1.getRight());
+      addNodeToStack(stack2, node2.getLeft());
+      addNodeToStack(stack2, node2.getRight());
+    }
+    return equals;
+  }
+
+  private void addNodeToStack(Stack<BinaryNode> stack, BinaryNode node) {
+    if (node != null) {
+      stack.add(node);
     }
   }
 }
--- src/test/java/com/github/pedrovgs/app/problem17/BinaryTreeEqualsTest.java
+++ src/test/java/com/github/pedrovgs/app/problem17/BinaryTreeEqualsTest.java
@@ -33,24 +33,25 @@ public class BinaryTreeEqualsTest {
     this.binaryTreeEquals = new BinaryTreeEquals();
   }
 
-  @Test(expected = IllegalArgumentException.class) public void shouldNotAcceptTwoNullTrees() {
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldNotAcceptTwoNullTreesRecursive() {
     binaryTreeEquals.areEqualsRecursive(null, null);
   }
 
-  @Test public void shouldReturnFalseIfOneIsNull() {
+  @Test public void shouldReturnFalseIfOneIsNullRecursive() {
     BinaryNode<Integer> root = new BinaryNode<Integer>(0);
 
     assertFalse(binaryTreeEquals.areEqualsRecursive(root, null));
   }
 
-  @Test public void shouldReturnTrueIfBothElementsAreEqualsEvenIfJustContainsOneElement() {
+  @Test public void shouldReturnTrueIfBothElementsAreEqualsEvenIfJustContainsOneElementRecursive() {
     BinaryNode<Integer> root = new BinaryNode<Integer>(0);
     BinaryNode<Integer> root1 = new BinaryNode<Integer>(0);
 
     assertTrue(binaryTreeEquals.areEqualsRecursive(root, root1));
   }
 
-  @Test public void shouldReturnFalseIfTreesAreNotEquals() {
+  @Test public void shouldReturnFalseIfTreesAreNotEqualsRecursive() {
     BinaryNode<Integer> root = new BinaryNode<Integer>(0);
     BinaryNode<Integer> n1 = new BinaryNode<Integer>(1);
     root.setLeft(n1);
@@ -59,7 +60,7 @@ public class BinaryTreeEqualsTest {
     assertFalse(binaryTreeEquals.areEqualsRecursive(root, root1));
   }
 
-  @Test public void shouldReturnTrueIfTreesAreEquals() {
+  @Test public void shouldReturnTrueIfTreesAreEqualsRecursive() {
     BinaryNode<Integer> root = new BinaryNode<Integer>(0);
     BinaryNode<Integer> n1 = new BinaryNode<Integer>(1);
     BinaryNode<Integer> n2 = new BinaryNode<Integer>(2);
@@ -77,5 +78,60 @@ public class BinaryTreeEqualsTest {
     n1.setLeft(n31);
 
     assertFalse(binaryTreeEquals.areEqualsRecursive(root, root1));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldNotAcceptTwoNullTreesIterative() {
+    binaryTreeEquals.areEqualsIterative(null, null);
+  }
+
+  @Test public void shouldReturnFalseIfOneIsNullIterative() {
+    BinaryNode<Integer> root = new BinaryNode<Integer>(0);
+
+    assertFalse(binaryTreeEquals.areEqualsIterative(root, null));
+  }
+
+  @Test public void shouldReturnTrueIfBothElementsAreEqualsEvenIfJustContainsOneElementIterative() {
+    BinaryNode<Integer> root = new BinaryNode<Integer>(0);
+    BinaryNode<Integer> root1 = new BinaryNode<Integer>(0);
+
+    assertTrue(binaryTreeEquals.areEqualsIterative(root, root1));
+  }
+
+  @Test public void shouldReturnFalseIfTreesAreNotEqualsIterative() {
+    BinaryNode<Integer> root = new BinaryNode<Integer>(0);
+    BinaryNode<Integer> n1 = new BinaryNode<Integer>(1);
+    BinaryNode<Integer> n2 = new BinaryNode<Integer>(2);
+    root.setLeft(n1);
+    root.setRight(n2);
+    BinaryNode<Integer> root1 = new BinaryNode<Integer>(0);
+    BinaryNode<Integer> n11 = new BinaryNode<Integer>(1);
+    BinaryNode<Integer> n21 = new BinaryNode<Integer>(2);
+    BinaryNode<Integer> n31 = new BinaryNode<Integer>(3);
+    root.setLeft(n11);
+    root.setRight(n21);
+    n21.setRight(n31);
+
+    assertFalse(binaryTreeEquals.areEqualsIterative(root, root1));
+  }
+
+  @Test public void shouldReturnTrueIfTreesAreEqualsIterative() {
+    BinaryNode<Integer> root = new BinaryNode<Integer>(0);
+    BinaryNode<Integer> n1 = new BinaryNode<Integer>(1);
+    BinaryNode<Integer> n2 = new BinaryNode<Integer>(2);
+    BinaryNode<Integer> n3 = new BinaryNode<Integer>(3);
+    root.setLeft(n1);
+    root.setRight(n2);
+    n1.setLeft(n3);
+
+    BinaryNode<Integer> root1 = new BinaryNode<Integer>(0);
+    BinaryNode<Integer> n11 = new BinaryNode<Integer>(1);
+    BinaryNode<Integer> n21 = new BinaryNode<Integer>(2);
+    BinaryNode<Integer> n31 = new BinaryNode<Integer>(3);
+    root.setLeft(n11);
+    root.setRight(n21);
+    n1.setLeft(n31);
+
+    assertFalse(binaryTreeEquals.areEqualsIterative(root, root1));
   }
 }
