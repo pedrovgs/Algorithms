e4f0eb7fd8c376edc1a3de145d28820f445bc3ef
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-29 15:17:45 +0100

    Add statement to problem 18

---
+++ src/main/java/com/github/pedrovgs/app/problem18/IsBST.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.app.problem18;
+
+/**
+ * Given a binary tree passed as argument, can you write a method to return true if the binary tree
+ * is a Binary Search Tree?
+ *
+ * A Binary Search Tree (BST), sometimes also called an ordered or sorted binary tree, is a
+ * node-based binary tree data structure where each node has a comparable key (and an associated
+ * value) and satisfies the restriction that the key in any node is larger than the keys in all
+ * nodes in that node's left sub-tree and smaller than the keys in all nodes in that node's right
+ * sub-tree. Each node has no more than two child nodes. Each child must either be a leaf node or
+ * the root of another binary search tree. The left sub-tree contains only nodes with keys less
+ * than
+ * the parent node; the right sub-tree contains only nodes with keys greater than the parent node.
+ * BSTs are also dynamic data structures, and the size of a BST is only limited by the amount of
+ * free memory in the operating system. The main advantage of binary search trees is that it
+ * remains
+ * ordered, which provides quicker search times than many other data structures.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class IsBST {
+}
