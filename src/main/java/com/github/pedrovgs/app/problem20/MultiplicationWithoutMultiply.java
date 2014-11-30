21fd4d15d077d02fa26b829811ce3eef27eea44b
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-01 00:45:20 +0100

    Add solution to problem 20

--- src/main/java/com/github/pedrovgs/app/problem20/MultiplicationWithoutMultiply.java
+++ src/main/java/com/github/pedrovgs/app/problem20/MultiplicationWithoutMultiply.java
@@ -23,8 +23,24 @@ package com.github.pedrovgs.app.problem20;
  */
 public class MultiplicationWithoutMultiply {
 
+  /**
+   * Iterative solution to this problem. This algorithm is based on the multiplication definition
+   * as
+   * a consecutive sum. The complexity order of this algorithm in time terms is O(N) where N is
+   * equals to n1 parameter. In space terms the complexity order of this algorithm is O(1) because
+   * we are not using any additional data structure related to the input size.
+   */
   public int calculate(int n1, int n2) {
-    return 0;
-  }
+    int result = 0;
+    boolean negative = n1 < 0;
+    n1 = Math.abs(n1);
+    for (int i = 0; i < n1; i++) {
+      result += n2;
+    }
 
+    if (negative) {
+      result *= -1;
+    }
+    return result;
+  }
 }
