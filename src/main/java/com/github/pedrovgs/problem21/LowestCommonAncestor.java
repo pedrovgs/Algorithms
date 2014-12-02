de3748998f612c462ad5351b7d5835bde9a3ca6e
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-02 19:56:53 +0100

    Add iterative solution to problem 21

--- src/main/java/com/github/pedrovgs/problem21/LowestCommonAncestor.java
+++ src/main/java/com/github/pedrovgs/problem21/LowestCommonAncestor.java
@@ -16,6 +16,8 @@
 package com.github.pedrovgs.problem21;
 
 import com.github.pedrovgs.binarytree.BinaryNode;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Given a BinaryTree and two nodes, can you write a method to find the lowest common ancestor of
@@ -52,5 +54,58 @@ public class LowestCommonAncestor {
       }
       return leftBranch != null ? leftBranch : rightBranch;
     }
+  }
+
+  /**
+   * Iterative solution for this problem. The complexity order of this algorithm is the same in
+   * memory terms, O(N), but is worst in space terms because we are using a list of BinaryNodes
+   * that is going to suppose O(N) for space terms where N is the path to the element.
+   *
+   * This algorithm is based on find the path of two nodes given a root node and compare both paths
+   * to find the first non common element and return the previous node, the lowest common ancestor.
+   */
+  public BinaryNode getIterative(BinaryNode root, BinaryNode n1, BinaryNode n2) {
+    if (root == null || n1 == null || n2 == null) {
+      throw new IllegalArgumentException("You can't pass null elements as input.");
+    }
+
+    List<BinaryNode> pathToA = getPathTo(root, n1);
+    List<BinaryNode> pathToB = getPathTo(root, n2);
+    BinaryNode result = null;
+    int size = Math.min(pathToA.size(), pathToB.size());
+    for (int i = 0; i < size; i++) {
+      if (pathToA.get(i) != pathToB.get(i)) {
+        result = pathToA.get(i - 1);
+        break;
+      }
+    }
+    return result;
+  }
+
+  private static List<BinaryNode> getPathTo(BinaryNode root, BinaryNode target) {
+
+    List<BinaryNode> path = new LinkedList<BinaryNode>();
+    if (root == target) {
+      path.add(target);
+    } else {
+      if (root.getLeft() == null && root.getRight() == null) {
+        return null;
+      }
+      if (root.getLeft() != null) {
+        List<BinaryNode> pathTo = getPathTo(root.getLeft(), target);
+        if (pathTo != null) {
+          path.add(root);
+          path.addAll(getPathTo(root.getLeft(), target));
+        }
+      }
+      if (root.getRight() != null) {
+        List<BinaryNode> pathTo = getPathTo(root.getRight(), target);
+        if (pathTo != null) {
+          path.add(root);
+          path.addAll(pathTo);
+        }
+      }
+    }
+    return path;
   }
 }
--- src/test/java/com/github/pedrovgs/problem21/LowestCommonAncestorTest.java
+++ src/test/java/com/github/pedrovgs/problem21/LowestCommonAncestorTest.java
@@ -32,26 +32,9 @@ public class LowestCommonAncestorTest {
     lca = new LowestCommonAncestor();
   }
 
-  @Test(expected = IllegalArgumentException.class) public void shouldNotAcceptNullElementsRecursive() {
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldNotAcceptNullElementsRecursive() {
     lca.getRecursive(null, null, null);
-  }
-
-  @Test public void shouldFindLCAWhenTheLCAIsOneOfTheNodesPassedAsInputRecursive() {
-    BinaryNode<Integer> root = new BinaryNode<Integer>(1);
-    BinaryNode<Integer> n2 = new BinaryNode<Integer>(2);
-    BinaryNode<Integer> n3 = new BinaryNode<Integer>(3);
-    BinaryNode<Integer> n4 = new BinaryNode<Integer>(4);
-    BinaryNode<Integer> n5 = new BinaryNode<Integer>(5);
-    BinaryNode<Integer> n6 = new BinaryNode<Integer>(6);
-    root.setLeft(n2);
-    root.setRight(n3);
-    n2.setLeft(n4);
-    n2.setRight(n5);
-    n4.setLeft(n6);
-
-    BinaryNode result = lca.getRecursive(root, root, n2);
-
-    assertEquals(root, result);
   }
 
   @Test public void shouldFindLCAWhenIsAFatherOfBothNodesRecursive() {
@@ -71,4 +54,28 @@ public class LowestCommonAncestorTest {
 
     assertEquals(n2, result);
   }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldNotAcceptNullElementsIterative() {
+    lca.getIterative(null, null, null);
+  }
+
+  @Test public void shouldFindLCAWhenIsAFatherOfBothNodesIterative() {
+    BinaryNode<Integer> root = new BinaryNode<Integer>(1);
+    BinaryNode<Integer> n2 = new BinaryNode<Integer>(2);
+    BinaryNode<Integer> n3 = new BinaryNode<Integer>(3);
+    BinaryNode<Integer> n4 = new BinaryNode<Integer>(4);
+    BinaryNode<Integer> n5 = new BinaryNode<Integer>(5);
+    BinaryNode<Integer> n6 = new BinaryNode<Integer>(6);
+    root.setLeft(n2);
+    root.setRight(n3);
+    n2.setLeft(n4);
+    n2.setRight(n5);
+    n4.setLeft(n6);
+
+    BinaryNode result = lca.getIterative(root, n5, n6);
+
+    assertEquals(n2, result);
+  }
 }
+
