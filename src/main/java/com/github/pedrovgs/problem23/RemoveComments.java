2fd174fad855802559efaa0f1acec5a8cd16a084
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-04 00:16:11 +0100

    Add iterative implementation to problem 23

--- src/main/java/com/github/pedrovgs/problem23/RemoveComments.java
+++ src/main/java/com/github/pedrovgs/problem23/RemoveComments.java
@@ -22,4 +22,48 @@ package com.github.pedrovgs.problem23;
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class RemoveComments {
+
+  private static final char ASTERISK = '*';
+  private static final char SLASH = '/';
+  private static final char ANY_CHAR = 'c';
+
+  /**
+   * Iterative solution to this problem. This algorithm has a complexity order in space terms
+   * equals to O(1) because we are not using any additional data structure to store temporal data.
+   * In time terms this algorithm has a complexity order equals to O(N) because we are going to
+   * check every single character inside the file.
+   *
+   * This algorithm could be easier to read if we replace the char checks for indexOf methods. But
+   * indexOf usage has a complexity order equals to O(N) and the result could be really slow.
+   */
+  public String remove(FakeFile file) {
+    if (file == null) {
+      throw new IllegalArgumentException("You can't pass a null file as argument.");
+    }
+
+    StringBuilder result = new StringBuilder();
+    boolean openComment = false;
+    String line = file.getLine();
+    while (line != null) {
+      char previous = ANY_CHAR;
+
+      for (char c : line.toCharArray()) {
+        if (openComment) {
+          if (c == SLASH && previous == ASTERISK) {
+            openComment = false;
+          }
+        } else {
+          if (c == ASTERISK && previous == SLASH) {
+            openComment = true;
+            result.deleteCharAt(result.length() - 1);
+          } else {
+            result.append(c);
+          }
+        }
+        previous = c;
+      }
+      line = file.getLine();
+    }
+    return result.toString();
+  }
 }
