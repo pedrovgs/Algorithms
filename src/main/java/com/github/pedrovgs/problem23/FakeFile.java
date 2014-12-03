11e375162fdcaa6fcd83b229d791dc92114239d5
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-04 00:09:33 +0100

    Add FakeFile implementation

---
+++ src/main/java/com/github/pedrovgs/problem23/FakeFile.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem23;
+
+/**
+ * Class created to simulate a File object for problem 23.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+class FakeFile {
+
+  private final String[] lines;
+  private int index;
+
+  FakeFile(String... lines) {
+    this.lines = lines;
+  }
+
+  String getLine() {
+    return index <= lines.length - 1 ? lines[index++] : null;
+  }
+}
