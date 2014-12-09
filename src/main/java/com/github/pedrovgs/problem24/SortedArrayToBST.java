8a96ae6bca0c9693121ca23a95a86e19485f58a9
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-09 23:42:17 +0100

    Add statement to problem 24

---
+++ src/main/java/com/github/pedrovgs/problem24/SortedArrayToBST.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem24;
+
+/**
+ * Given a sorted array full of integers, can you write a method to return a BST?
+ *
+ * A binary search tree (BST), sometimes also called an ordered or sorted binary tree, is a
+ * node-based binary tree data structure where each node has a comparable key (and an associated
+ * value) and satisfies the restriction that the key in any node is larger than the keys in all
+ * nodes in that node's left sub-tree are smaller than the keys in all nodes in that node's right
+ * sub-tree. Each node has no more than two child nodes.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class SortedArrayToBST {
+}
