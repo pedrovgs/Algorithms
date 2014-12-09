e18762d76cc9a623ceb83bdce944c2de7ac1c657
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-10 00:20:25 +0100

    Add statement to problem 25

---
+++ src/main/java/com/github/pedrovgs/problem25/Anagrams.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem25;
+
+/**
+ * Given a String passed as parameter, can you write a method to calculate input word anagrams?
+ *
+ * An anagram is a type of word play, the result of rearranging the letters of a word or phrase to
+ * produce a new word or phrase, using all the original letters exactly once; for example Torchwood
+ * can be rearranged into Doctor Who. The original word or phrase is known as the subject of the
+ * anagram. Anagrams are often used as a form of mnemonic device as well.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class Anagrams {
+}
