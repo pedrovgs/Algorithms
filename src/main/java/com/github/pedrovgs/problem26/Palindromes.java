3928cba10f0d5f6dc9b97d72842d2deae7fa2677
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-11 21:09:53 +0100

    Add statement to problem 26

---
+++ src/main/java/com/github/pedrovgs/problem26/Palindromes.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem26;
+
+/**
+ * Given a String as input, can you write a method to check if that String is a palindrome?
+ *
+ * A palindrome is a word, phrase, number, or other sequence of characters which reads the same
+ * backward or forward. Allowances may be made for adjustments to capital letters, punctuation, and
+ * word dividers.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class Palindromes {
+}
