a74b4b0ad8fa3ecb1e9dce236309fc1b21d6cb1a
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-13 00:47:26 +0100

    Add statement to problem 28

---
+++ src/main/java/com/github/pedrovgs/problem28/FindSums.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem28;
+
+/**
+ * Given an array of numbers and a Integer as input, can you return a list of pairs in the input
+ * array that can sum the input value.
+ *
+ * For example: Input 7,{5,2,6,1,9} Output {5,2},{6,1}
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class FindSums {
+}
