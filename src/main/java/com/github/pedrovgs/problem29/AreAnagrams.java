d690539a96d94d0c98fcaeabb1615729373cdede
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-14 14:13:11 +0100

    Add iterative solution to problem 29

--- src/main/java/com/github/pedrovgs/problem29/AreAnagrams.java
+++ src/main/java/com/github/pedrovgs/problem29/AreAnagrams.java
@@ -28,7 +28,45 @@ package com.github.pedrovgs.problem29;
  */
 public class AreAnagrams {
 
-  public boolean check(String word1, String word2) {
-    return false;
+  /**
+   * Iterative algorithm to solve this problem. Two words are anagrams just if contains the same
+   * number of letters. Using this property, we are going to calculate the number of letters per
+   * word and check if the sum is the same. We have used an array of int to represent the letters
+   * counter. The complexity order of this algorithm is O(N) where N is the number of letters in
+   * the largest word. In space terms, the complexity order of this algorithm is O(M), where M is
+   * the number of different letters in the alphabet (we assume ascii, 128 different letters),
+   * because we need an additional data structure to store information related to the letters
+   * counter.
+   */
+  public boolean check(String a, String b) {
+    if (a == null || b == null) {
+      throw new IllegalArgumentException("You can't use null Strings as input.");
+    }
+    if (a.length() != b.length()) {
+      return false;
+    }
+    int[] lettersA = getLettersAsArray(a);
+    int[] lettersB = getLettersAsArray(b);
+
+    int valuesA = getSumOfArray(lettersA);
+    int valuesB = getSumOfArray(lettersB);
+
+    return valuesA == valuesB;
+  }
+
+  private static int getSumOfArray(int[] array) {
+    int count = 0;
+    for (int n : array) {
+      count += n;
+    }
+    return count;
+  }
+
+  private static int[] getLettersAsArray(String word) {
+    int[] letters = new int[128];
+    for (char c : word.toCharArray()) {
+      letters[c] = letters[c] + 1;
+    }
+    return letters;
   }
 }
