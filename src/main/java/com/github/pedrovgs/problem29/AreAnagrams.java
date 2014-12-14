ba92e0468c9a0099dfa40b89132a1f582e13c69e
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-14 14:03:56 +0100

    Add statement to problem 29

---
+++ src/main/java/com/github/pedrovgs/problem29/AreAnagrams.java
@@ -0,0 +1,15 @@
+package com.github.pedrovgs.problem29;
+
+/**
+ * Given two Strings passed as parameter, can you write a method to return true if this words are
+ * anagrams?
+ *
+ * For example:
+ *
+ * Input: ana,naa Output: true
+ * Input: pedro,ana Output: false
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class AreAnagrams {
+}
