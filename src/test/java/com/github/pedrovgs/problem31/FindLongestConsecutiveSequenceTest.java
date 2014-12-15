261c263fe40baff31a46b4f19f0099ab6015b3d7
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-16 00:44:04 +0100

    Move test to the correct package.

--- src/test/java/com/github/pedrovgs/problem31/FindLongestConsecutiveSequenceTest.java
+++ src/test/java/com/github/pedrovgs/problem31/FindLongestConsecutiveSequenceTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.pedrovgs;
+package com.github.pedrovgs.problem31;
 
 import com.github.pedrovgs.problem31.FindLongestConsecutiveSequence;
 import org.junit.Before;
