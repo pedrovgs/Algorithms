e234e2db7e61886fcb8f825c0211d13f7f2fb491
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-16 00:34:18 +0100

    Add statement to problem 31

---
+++ src/main/java/com/github/pedrovgs/problem31/FindLongestConsecutiveSequence.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem31;
+
+/**
+ * Given an array full of integers, can you write a method to find the length of the longest
+ * consecutive sequence inside the array?
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class FindLongestConsecutiveSequence {
+}
