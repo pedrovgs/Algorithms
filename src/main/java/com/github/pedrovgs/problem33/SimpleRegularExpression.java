dc929a03f8f3ca3b08a6e8a67cea9a3ed5a8804e
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-18 00:21:53 +0100

    Add statement to problem 33

---
+++ src/main/java/com/github/pedrovgs/problem33/SimpleRegularExpression.java
@@ -0,0 +1,12 @@
+package com.github.pedrovgs.problem33;
+
+/**
+ * Can you write a method to analyze some strings with regular expressions and return true if the
+ * expression matches with the word? Expressions supported are:
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class SimpleRegularExpression {
+}
