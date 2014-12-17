5f3d5bd5de9ee50b7aa73c7b55b3d205dd2f0a67
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-17 23:20:33 +0100

    Add iterative solution to problem 32

--- src/main/java/com/github/pedrovgs/problem32/Contains.java
+++ src/main/java/com/github/pedrovgs/problem32/Contains.java
@@ -23,7 +23,27 @@ package com.github.pedrovgs.problem32;
  */
 public class Contains {
 
-  public boolean evaluate(String word1, String word2) {
-    return false;
+  /**
+   * Iterative solution to this problem. The complexity order of this algorithm is O(N*M) where N
+   * is the length of the first word and M is the length of the second word. In space terms the
+   * complexity order of this algorithm is O(1) because we are not using any auxiliary data
+   * structure to solve this problem.
+   */
+  public boolean evaluate(String w1, String w2) {
+    if (w1 == null || w2 == null) {
+      throw new IllegalArgumentException("You can't pass null strings as input.");
+    }
+    boolean contains = false;
+    for (int i = 0; i < w2.length() - 1; i++) {
+      if (w2.charAt(i) == w1.charAt(0)) {
+        for (int j = 0; j < w1.length(); j++) {
+          if (w1.charAt(j) == w2.charAt(i + j) && j == w1.length() - 1) {
+            contains = true;
+            break;
+          }
+        }
+      }
+    }
+    return contains;
   }
 }
