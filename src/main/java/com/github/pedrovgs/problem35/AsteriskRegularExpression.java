e31bceff568d7efe09f176d78248265f369e9f7a
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-20 14:17:49 +0100

    Add statement to problem 35

---
+++ src/main/java/com/github/pedrovgs/problem35/AsteriskRegularExpression.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem35;
+
+/**
+ * Given an array of strings, can you write a method to return return just words that matches with
+ * a pattern containing the "*" regular expression.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class AsteriskRegularExpression {
+  
+}
