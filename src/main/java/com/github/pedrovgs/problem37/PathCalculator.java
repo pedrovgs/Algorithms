6327840345060132cc6ce422f4d8f33b77d18031
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-22 21:30:53 +0100

    Add iterative solution to problem 37

--- src/main/java/com/github/pedrovgs/problem37/PathCalculator.java
+++ src/main/java/com/github/pedrovgs/problem37/PathCalculator.java
@@ -15,11 +15,56 @@
  */
 package com.github.pedrovgs.problem37;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * Given an absolute path and two relative paths to a directory, can you write a method to calculate
+ * Given an absolute path and two relative paths to a directory, can you write a method to
+ * calculate
  * the final path?
  *
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class PathCalculator {
+
+  public static final String DIR_SEPARATOR = "/";
+
+  /**
+   * Iterative algorithm to solve this problem. The complexity order of this algorithm is O(N+M+Ñ)
+   * where N,M and Ñ are the length of every path. We are going to transform string path to arrays
+   * and evaluate every one in a different way adding and subtracting elements from a list.
+   */
+  public String calculate(String absolute, String r1, String r2) {
+    if (absolute == null || r1 == null || r2 == null) {
+      throw new IllegalArgumentException("You can't pass null strings as input.");
+    }
+
+    String[] absolutePath = absolute.split(DIR_SEPARATOR);
+    String[] r1Path = r1.split(DIR_SEPARATOR);
+    String[] r2Path = r2.split(DIR_SEPARATOR);
+    String[] result = calculatePath(absolutePath, r1Path, r2Path);
+
+    StringBuilder stringBuilder = new StringBuilder(DIR_SEPARATOR);
+    for (String dir : result) {
+      stringBuilder.append(dir);
+      stringBuilder.append("/");
+    }
+    return stringBuilder.toString();
+  }
+
+  private static String[] calculatePath(String[]... paths) {
+    List<String> finalPath = new LinkedList<String>();
+    for (String[] path : paths) {
+      for (String dir : path) {
+        if (dir.equals("..")) {
+          if (!finalPath.isEmpty()) {
+            finalPath.remove(finalPath.size() - 1);
+          }
+        } else if (!dir.isEmpty()) {
+          finalPath.add(dir);
+        }
+      }
+    }
+    return finalPath.toArray(new String[finalPath.size()]);
+  }
 }
