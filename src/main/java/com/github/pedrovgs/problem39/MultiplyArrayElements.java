442a6caceb932401c12e4dcb2388c57fe6a30349
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-24 15:49:04 +0100

    Add iterative solution to problem 39

--- src/main/java/com/github/pedrovgs/problem39/MultiplyArrayElements.java
+++ src/main/java/com/github/pedrovgs/problem39/MultiplyArrayElements.java
@@ -20,4 +20,34 @@ package com.github.pedrovgs.problem39;
  * return a new array with this values. Can you resolve this problem without use "/" operator?
  */
 public class MultiplyArrayElements {
+
+  /**
+   * Iterative solution to solve this problem. The complexity order of this algorithm is O(N) time
+   * and space terms where N is the number of elements in the input array.
+   */
+  public int[] multiply(int[] input) {
+    if (input == null) {
+      throw new IllegalArgumentException("You can't pass a null array as input.");
+    }
+    if (input.length == 0) {
+      return input;
+    }
+
+    int[] front = new int[input.length];
+    int[] rear = new int[input.length];
+    int[] output = new int[input.length];
+
+    front[0] = 1;
+    rear[input.length - 1] = 1;
+    for (int i = 1; i < input.length; i++) {
+      front[i] = front[i - 1] * input[i - 1];
+    }
+    for (int i = input.length - 2; i >= 0; i--) {
+      rear[i] = rear[i + 1] * input[i + 1];
+    }
+    for (int i = 0; i < input.length; i++) {
+      output[i] = front[i] * rear[i];
+    }
+    return output;
+  }
 }
