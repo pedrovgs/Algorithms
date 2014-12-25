36ce5975f62b327ffca49ed5cfcf5262c837c398
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-25 19:18:23 +0100

    Add statement to problem 40

---
+++ src/main/java/com/github/pedrovgs/problem40/MoveElementsToPositions.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem40;
+
+/**
+ * Given an array with elements from 1 to N where N is the size of the array operate with the
+ * array to put every element in the position marked with the value of the element. You can't use
+ * any other data structure and take into account the final result has to reflex the values of
+ * the original one, not the already modified one.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class MoveElementsToPositions {
+}
