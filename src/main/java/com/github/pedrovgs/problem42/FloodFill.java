7fd4ba72863d09c2855a6df1b70351f730ab4a1b
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-27 13:26:23 +0100

    Add statement to problem 42

---
+++ src/main/java/com/github/pedrovgs/problem42/FloodFill.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem42;
+
+/**
+ * Given a matrix full of 1's and 0's can you write a method to fill part of the matrix using a
+ * number?
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class FloodFill {
+}
