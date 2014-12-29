3cb6e1093986b9545f081bb5fe243713ce44a670
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-29 16:18:52 +0100

    Add solution to problem 44

--- src/main/java/com/github/pedrovgs/problem44/FindMinNumberAtPosition.java
+++ src/main/java/com/github/pedrovgs/problem44/FindMinNumberAtPosition.java
@@ -15,10 +15,52 @@
  */
 package com.github.pedrovgs.problem44;
 
+import com.github.pedrovgs.binarytree.BinaryNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Given a Binary Search Tree, can you write a method to obtain the nTH min element?
  *
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class FindMinNumberAtPosition {
+
+  /**
+   * This algorithm has a complexity order in space and time terms of O(N) where N is the number of
+   * elements in the tree. It's based on the property of BST. If you go through a BST using a
+   * in order tour you'll get an ordered list of elements.
+   */
+  public Integer find(BinaryNode<Integer> root, int position) {
+    validateInputData(root, position);
+    List<BinaryNode<Integer>> orderedElements = getOrderedElements(root);
+    validatePosition(position, orderedElements);
+    return orderedElements.get(position - 1).getData();
+  }
+
+  private static List<BinaryNode<Integer>> getOrderedElements(BinaryNode root) {
+    if (root == null) {
+      return new ArrayList<BinaryNode<Integer>>();
+    } else {
+      List<BinaryNode<Integer>> nodes = new LinkedList<BinaryNode<Integer>>();
+      nodes.addAll(getOrderedElements(root.getLeft()));
+      nodes.add(root);
+      nodes.addAll(getOrderedElements(root.getRight()));
+      return nodes;
+    }
+  }
+
+  private void validatePosition(int position, List<BinaryNode<Integer>> orderedElements) {
+    if (position >= orderedElements.size()) {
+      throw new IllegalArgumentException(
+          "The number of elements in the tree is minor than the position requested.");
+    }
+  }
+
+  private void validateInputData(BinaryNode<Integer> root, int position) {
+    if (root == null || position < 0) {
+      throw new IllegalArgumentException("You can't pass null binary trees or negative positions.");
+    }
+  }
 }
