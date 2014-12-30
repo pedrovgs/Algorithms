6dcccf658d395c053dd994cac7d3aa65f1b9c258
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-30 13:25:15 +0100

    Add iterative solution to problem 45

--- src/main/java/com/github/pedrovgs/problem45/FindNthMostRepeatedElement.java
+++ src/main/java/com/github/pedrovgs/problem45/FindNthMostRepeatedElement.java
@@ -15,10 +15,53 @@
  */
 package com.github.pedrovgs.problem45;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array full of integers, can you write a method to find the element repeated kTH times.
  *
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class FindNthMostRepeatedElement {
+
+  /**
+   * Iterative solution to solve this problem. This algorithm is based on a complexity order equals
+   * to O(N) and space O(N) because you need to go through the full set numbers two times and you
+   * need a map to store the number of repetitions for every element.
+   */
+  public int find(int[] numbers, int position) {
+    validateInput(numbers, position);
+    Integer result = null;
+    Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
+    for (int i : numbers) {
+      if (counter.get(i) == null) {
+        counter.put(i, 1);
+      } else {
+        counter.put(i, counter.get(i) + 1);
+      }
+    }
+    for (Integer candidate : counter.keySet()) {
+      if (counter.get(candidate) == position) {
+        result = candidate;
+        break;
+      }
+    }
+    validateResult(result);
+    return result;
+  }
+
+  private void validateInput(int[] numbers, int position) {
+    if (numbers == null || position <= 0) {
+      throw new IllegalArgumentException(
+          "You can't pass null arrays or position values minor than 0 as parameter.");
+    }
+  }
+
+  private void validateResult(Integer result) {
+    if (result == null) {
+      throw new IllegalArgumentException(
+          "There are no elements repeated n times in the array passed as argument");
+    }
+  }
 }
