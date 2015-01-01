9b061197f8c89eb2a27479e5d8ec15ed3516b6ad
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-01 12:58:42 +0100

    Add iterative solution to problem 47

--- src/main/java/com/github/pedrovgs/problem47/ReturnChange.java
+++ src/main/java/com/github/pedrovgs/problem47/ReturnChange.java
@@ -15,6 +15,7 @@
  */
 package com.github.pedrovgs.problem47;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -24,7 +25,30 @@ import java.util.List;
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class ReturnChange {
+
+  /**
+   * Iterative solution to this algorithm. This algorithm has a complexity order equals to O(N)
+   * where N is the billAmount/maxMoneyValue. At the end, linear to the value passed as parameter.
+   */
   public List<Integer> calculate(int[] moneys, int billAmount) {
-    return null;
+    validateInputData(moneys, billAmount);
+    List<Integer> change = new LinkedList<Integer>();
+    while (billAmount > 0 && moneys.length > 0) {
+      for (int i = moneys.length - 1; i >= 0; i--) {
+        if (billAmount - moneys[i] >= 0) {
+          change.add(moneys[i]);
+          billAmount -= moneys[i];
+          break;
+        }
+      }
+    }
+    return change;
+  }
+
+  private void validateInputData(int[] moneys, int billAmount) {
+    if (moneys == null || moneys.length == 0 || billAmount <= 0) {
+      throw new IllegalArgumentException(
+          "Review your parameters used as input, you can't use a null or empty array of moneys or a bill amount equals or less than zero.");
+    }
   }
 }
