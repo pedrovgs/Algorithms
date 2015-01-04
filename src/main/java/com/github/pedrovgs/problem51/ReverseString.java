313da800dabefbaa3cff5b5d4bb9b32a7cf1181d
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-04 23:49:55 +0100

    Add iterative solution to problem 51

--- src/main/java/com/github/pedrovgs/problem51/ReverseString.java
+++ src/main/java/com/github/pedrovgs/problem51/ReverseString.java
@@ -21,4 +21,26 @@ package com.github.pedrovgs.problem51;
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class ReverseString {
+
+  /**
+   * Iterative algorithm to solve this problem. The complexity order in time and space terms of
+   * this algorithm is O(N) where N is the number of chars in the input String. In time terms is
+   * possible to get O(N) because String class uses a char array internally and the complexity
+   * order of charAt method is O(1).
+   */
+  public String reverse(String input) {
+    validateInput(input);
+
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int i = input.length() - 1; i >= 0; i--) {
+      stringBuilder.append(input.charAt(i));
+    }
+    return stringBuilder.toString();
+  }
+
+  private void validateInput(String input) {
+    if (input == null) {
+      throw new IllegalArgumentException("You can't pass a null String as input parameter.");
+    }
+  }
 }
