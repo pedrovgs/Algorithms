bbc875b76e3c83a69bacf0aaadf5d6b32622d953
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-06 00:48:31 +0100

    Add statement to problem 53

---
+++ src/main/java/com/github/pedrovgs/problem53/CompressString.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem53;
+
+/**
+ * Implement a method to perform basic string compression using the counts of repeated characters.
+ * For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not
+ * become smaller than the original string, your method should return the original string.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class CompressString {
+}
