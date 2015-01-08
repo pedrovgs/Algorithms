a9496cef5a20a5e02c1eef6fced6651ad5fd9512
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-09 00:15:54 +0100

    Add iterative solution to problem 56

--- src/main/java/com/github/pedrovgs/problem56/IsRotationUsingIsSubstring.java
+++ src/main/java/com/github/pedrovgs/problem56/IsRotationUsingIsSubstring.java
@@ -24,4 +24,38 @@ package com.github.pedrovgs.problem56;
  */
 public class IsRotationUsingIsSubstring {
 
+  /**
+   * Iterative algorithm used to solve this problem. The complexity order of this algorithm in time
+   * and space terms is O(N) where N is the sum of input strings lengths. To solve this problem we
+   * check if the length of input strings is the same and for strings with the same length we are
+   * going to concatenate s1 with s1 check if duplicated s1 contains s2.
+   *
+   * s1 = waterbottle
+   * s2 = erbotlewat
+   * s1s1 = waterbottlewaterbottle
+   * return "waterbottlewaterbottle".contains(erbotlewat)
+   */
+  public boolean check(String s1, String s2) {
+    validateInput(s1, s2);
+
+    if (haveSameLength(s1, s2)) {
+      String s1s1 = s1 + s1;
+      return isSubstring(s1s1, s2);
+    }
+    return false;
+  }
+
+  private boolean isSubstring(String s1s2, String s2) {
+    return s1s2.contains(s2);
+  }
+
+  private void validateInput(String s1, String s2) {
+    if (s1 == null || s2 == null) {
+      throw new IllegalArgumentException("You can't pass null instances of s1 or s2 as parameter.");
+    }
+  }
+
+  private boolean haveSameLength(String s1, String s2) {
+    return s1.length() == s2.length();
+  }
 }
