9f7771388a3f83323163b905c33f1ec0e61673ac
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-10 01:12:34 +0100

    Add iterative solution to problem 57

--- src/main/java/com/github/pedrovgs/problem57/RemoveListDuplicatedElements.java
+++ src/main/java/com/github/pedrovgs/problem57/RemoveListDuplicatedElements.java
@@ -15,6 +15,10 @@
  */
 package com.github.pedrovgs.problem57;
 
+import com.github.pedrovgs.linkedlist.ListNode;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Write code to remove duplicates from an unsorted linked list. How would you solve this problem
  * if
@@ -24,4 +28,42 @@ package com.github.pedrovgs.problem57;
  */
 public class RemoveListDuplicatedElements {
 
+  /**
+   * Iterative algorithm to solve this problem. The complexity order of this algorithm in time and
+   * space terms is O(N) where N is equals to the number of elements in the linked list.
+   */
+  public void remove(ListNode<Integer> head) {
+    validateInput(head);
+
+    Set<Integer> elements = new HashSet<Integer>();
+    ListNode<Integer> currentNode = head;
+    ListNode<Integer> prevNode = null;
+    while (currentNode != null) {
+      if (elements.contains(currentNode.getData())) {
+        prevNode.setNext(currentNode.getNext());
+      } else {
+        elements.add(currentNode.getData());
+      }
+      prevNode = currentNode;
+      currentNode = currentNode.getNext();
+    }
+  }
+
+  private void validateInput(ListNode head) {
+    if (head == null) {
+      throw new IllegalArgumentException("You can't pass a null ListNode as instance.");
+    }
+  }
+
+  private void removeNextNodesWithValue(ListNode<Integer> srcNode) {
+    ListNode<Integer> prevNode = srcNode;
+    ListNode<Integer> currentNode = srcNode.getNext();
+    while (currentNode != null) {
+      if (currentNode.getData().equals(srcNode.getData())) {
+        prevNode.setNext(currentNode.getNext());
+      }
+      prevNode = currentNode;
+      currentNode = currentNode.getNext();
+    }
+  }
 }
