73ecaa592ff742ef8c527fd9ca83d944343ed44b
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-11 15:43:48 +0100

    Add iterative solution to problem 58

--- src/main/java/com/github/pedrovgs/problem58/FindKthElement.java
+++ src/main/java/com/github/pedrovgs/problem58/FindKthElement.java
@@ -15,6 +15,9 @@
  */
 package com.github.pedrovgs.problem58;
 
+import com.github.pedrovgs.linkedlist.ListNode;
+import com.github.pedrovgs.problem22.ReverseLinkedList;
+
 /**
  * Implement an algorithm to find the kth to last element of a singly linked list. Can you do it
  * using recursion?
@@ -23,4 +26,40 @@ package com.github.pedrovgs.problem58;
  */
 public class FindKthElement {
 
+  private ReverseLinkedList reverseLinkedList;
+
+  public FindKthElement() {
+    this.reverseLinkedList = new ReverseLinkedList();
+  }
+
+  /**
+   * Iterative algorithm based on reverse the singly linked list and iterate over the reversed list
+   * to find the kth element. This algorithm has a complexity order in time terms equals
+   * to O(N) where N is the number of elements in the list. Remember that our reverse algorithm
+   * also has O(N) as complexity order. In space terms we get O(1) because we are not using any
+   * auxiliary data structure.
+   */
+  public ListNode find(ListNode listNode, int k) {
+    validateInput(listNode, k);
+
+    ListNode reversedList = reverseLinkedList.reverseIterative(listNode);
+    ListNode result = reversedList;
+    while (k > 0) {
+      result = result.getNext();
+      if (result == null) {
+        throw new IndexOutOfBoundsException();
+      }
+      k--;
+    }
+    return result;
+  }
+
+  private void validateInput(ListNode listNode, int k) {
+    if (listNode == null) {
+      throw new IllegalArgumentException("You can't pass a null ListNode as argument.");
+    }
+    if (k < 0) {
+      throw new IllegalArgumentException("You can't pass a negative position.");
+    }
+  }
 }
