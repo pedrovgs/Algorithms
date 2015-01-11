ea5beaf9616b7891e7ea375840accaeb3ed5c08b
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-12 00:16:53 +0100

    Add solution to problem 59

--- src/main/java/com/github/pedrovgs/problem59/DeleteListNode.java
+++ src/main/java/com/github/pedrovgs/problem59/DeleteListNode.java
@@ -15,6 +15,8 @@
  */
 package com.github.pedrovgs.problem59;
 
+import com.github.pedrovgs.linkedlist.ListNode;
+
 /**
  * Implement an algorithm to delete a node in the middle of a singly linked list, given only access
  * to that node.
@@ -23,4 +25,28 @@ package com.github.pedrovgs.problem59;
  */
 public class DeleteListNode {
 
+  /**
+   * Iterative algorithm to solve this problem. We don't have access to the head of the list, and
+   * the only thing we can do is to move the data from the next node to the current one, the data
+   * and the pointer to the next node. This approach doesn't work with the last element in the
+   * list, but there is no another valid approach to solve this algorithm without access to the
+   * head of the list.
+   */
+  public void delete(ListNode node) {
+    validateInputData(node);
+
+    if (node.getNext() == null) {
+      return;
+    }
+    ListNode next = node.getNext();
+    node.setData(next.getData());
+    node.setNext(next.getNext());
+  }
+
+  private void validateInputData(ListNode node) {
+    if (node == null) {
+      throw new IllegalArgumentException(
+          "You can't pass a null instance of ListNode as parameter.");
+    }
+  }
 }
