dd25e1b09e0a5985b78279c11ba315df7d95928d
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-14 00:58:22 +0100

    Add solution to the second part of problem 61

--- src/main/java/com/github/pedrovgs/problem61/SumLists.java
+++ src/main/java/com/github/pedrovgs/problem61/SumLists.java
@@ -57,7 +57,40 @@ public class SumLists {
     }
     return value;
   }
-  
+
+  /**
+   * Iterative solution to solve this problem. The complexity order is the same than in the
+   * previous algorithm in time and space terms, but the execution time is bigger here because we
+   * need to know the list size to be able to transform the list into an integer.
+   */
+  public int sum(ListNode<Integer> n1, ListNode<Integer> n2) {
+    validateInput(n1, n2);
+
+    int n1Value = calculateNumber(n1);
+    int n2Value = calculateNumber(n2);
+    return n1Value + n2Value;
+  }
+
+  private int calculateNumber(ListNode<Integer> list) {
+    int value = 0;
+    int digits = getListSize(list) - 1;
+    while (list != null) {
+      value += list.getData() * Math.pow(10, digits);
+      list = list.getNext();
+      digits--;
+    }
+    return value;
+  }
+
+  private int getListSize(ListNode<Integer> list) {
+    int size = 0;
+    while (list != null) {
+      list = list.getNext();
+      size++;
+    }
+    return size;
+  }
+
   private void validateInput(ListNode<Integer> n1, ListNode<Integer> n2) {
     if (n1 == null || n2 == null) {
       throw new IllegalArgumentException("You can't pass a null list as parameter.");
