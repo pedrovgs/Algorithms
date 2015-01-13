f4d4ab8adaba250e990ece5c92eab0670fd92c16
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-14 00:56:15 +0100

    Add statement to problem 61

---
+++ src/main/java/com/github/pedrovgs/problem61/SumLists.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem61;
+
+/**
+ * You have two numbers represented by a linked list, where each node contains a single digit.The
+ * digit are stored in reverse order,such that the 1's digit is at the head of the list. Write a
+ * function that adds the two numbers and returns the sum as a linked list.
+ *
+ * Suppose the digits are stored in forward order. Repeat the above problem.
+ *
+ * Negative numbers are not allowed.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class SumLists {
+
+}
