989876f9a15ce99b38a4ac68ed530fdb7e2664da
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-16 07:50:51 +0100

    Add more tests t problem 63

--- src/test/java/com/github/pedrovgs/problem63/ConstantComplexityOrderStackTest.java
+++ src/test/java/com/github/pedrovgs/problem63/ConstantComplexityOrderStackTest.java
@@ -20,6 +20,8 @@ import com.github.pedrovgs.problem63.ConstantComplexityOrderStack.Stack2;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author Pedro Vicente Gómez Sánchez.
  */
@@ -50,5 +52,24 @@ public class ConstantComplexityOrderStackTest {
     stack1.pop();
 
     assertEquals(1, stack1.getMin());
+  }
+
+  @Test public void shouldReturnMinElementInTheStack2() {
+    stack2.push(1);
+    stack2.push(2);
+    stack2.push(3);
+    stack2.push(-1);
+
+    assertEquals(-1, stack2.getMin());
+  }
+
+  @Test public void shouldReturnMinElementInTheStack2EvenAfterModifyStackContent() {
+    stack2.push(1);
+    stack2.push(2);
+    stack2.push(3);
+    stack2.push(-1);
+    stack2.pop();
+
+    assertEquals(1, stack2.getMin());
   }
 }
