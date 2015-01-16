0b05b0dbf72651092cfdbfe56003384bf58d64fd
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-16 07:49:35 +0100

    Add first implementation to problem 63

--- src/main/java/com/github/pedrovgs/problem63/ConstantComplexityOrderStack.java
+++ src/main/java/com/github/pedrovgs/problem63/ConstantComplexityOrderStack.java
@@ -15,6 +15,8 @@
  */
 package com.github.pedrovgs.problem63;
 
+import java.util.Stack;
+
 /**
  * How would you design an integer stack which, in addition to push and pop, also has a function
  * min which returns the minimum element? Push, pop and min should all operate in 0(1) time.
@@ -23,4 +25,35 @@ package com.github.pedrovgs.problem63;
  */
 public class ConstantComplexityOrderStack {
 
+  /**
+   * Approach based on a inner data structure to store the stack of integers and in the same object
+   * the current min value we had when the element was pushed. With this approach we get a time
+   * complexity order equals to O(1) but a space complexity order equals to O(N) where N is the
+   * number of elements in the stack.
+   */
+  public static class Stack1 extends Stack<MinValue> {
+
+    public void push(int item) {
+      int newMin = Math.min(item, getMin());
+      super.push(new MinValue(item, newMin));
+    }
+
+    public int getMin() {
+      if (isEmpty()) {
+        return Integer.MAX_VALUE;
+      } else {
+        return peek().minValue;
+      }
+    }
+  }
+
+  static class MinValue {
+    final int value;
+    final int minValue;
+
+    MinValue(int value, int minValue) {
+      this.value = value;
+      this.minValue = minValue;
+    }
+  }
 }
