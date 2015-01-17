c59bc2992f94c91d6865336b30d63d4951a8430b
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-17 16:06:02 +0100

    Add solution to problem 64

--- src/main/java/com/github/pedrovgs/problem64/GetTheElementInTheMiddleOfTheList.java
+++ src/main/java/com/github/pedrovgs/problem64/GetTheElementInTheMiddleOfTheList.java
@@ -15,6 +15,8 @@
  */
 package com.github.pedrovgs.problem64;
 
+import com.github.pedrovgs.linkedlist.ListNode;
+
 /**
  * Given a singly linked list, can you write a method to return the element in the middle of the
  * list?
@@ -23,4 +25,38 @@ package com.github.pedrovgs.problem64;
  */
 public class GetTheElementInTheMiddleOfTheList {
 
+  /**
+   * Iterative solution to this problem. The complexity order of this algorithm in time terms is
+   * equals to O(N) where N is the number of elements in the list. In space terms, the complexity
+   * order is O(1) because we are not using any auxiliary data structure to solve this problem.
+   * This algorithm is based on know the size of the list, but this part of the algorithm is a
+   * problem for the execution time.
+   */
+  public ListNode<Integer> get(ListNode<Integer> list) {
+    validateInput(list);
+
+    int size = getListSize(list);
+    int middleElementPosition = size % 2 != 0 ? size / 2 : (size / 2) - 1;
+    ListNode middleElement = list;
+    for (int i = 0; i < middleElementPosition; i++) {
+      middleElement = middleElement.getNext();
+    }
+    return middleElement;
+  }
+
+  private int getListSize(ListNode<Integer> list) {
+    int size = 0;
+    ListNode currentNode = list;
+    while (currentNode != null) {
+      size++;
+      currentNode = currentNode.getNext();
+    }
+    return size;
+  }
+
+  private void validateInput(ListNode list) {
+    if (list == null) {
+      throw new IllegalArgumentException("You can't pass a null instance of list");
+    }
+  }
 }
