a13dd4713cb1d21ca5687f49fbbda5166f0c5f65
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-19 19:37:48 +0100

    Add solution to problem 65

--- src/main/java/com/github/pedrovgs/problem65/IsTreeBalanced.java
+++ src/main/java/com/github/pedrovgs/problem65/IsTreeBalanced.java
@@ -15,6 +15,8 @@
  */
 package com.github.pedrovgs.problem65;
 
+import com.github.pedrovgs.binarytree.BinaryNode;
+
 /**
  * Implement a function to check if a binary tree is balanced. For the purposes of this question, a
  * balanced tree is defined to be a tree such that the heights of the two subtrees of any node
@@ -23,5 +25,28 @@ package com.github.pedrovgs.problem65;
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class IsTreeBalanced {
-  
+
+  /**
+   * Recursive algorithm to check if a binary tree is balanced. The complexity order in time terms
+   * of this algorithm is equals to O(N) where N is the number of nodes in the binary tree. In
+   * space terms, the complexity order is O(1) because we are not using any auxiliary data
+   * structure to resolve this problem. This algorithm is based on the tree height because the
+   * balanced tree definition is based on this.
+   */
+  public boolean check(BinaryNode root) {
+    if (root == null) {
+      return true;
+    } else {
+      int heightDifference = getRootHeight(root.getLeft()) - getRootHeight(root.getRight());
+      return Math.abs(heightDifference) <= 1;
+    }
+  }
+
+  private int getRootHeight(BinaryNode tree) {
+    if (tree == null) {
+      return 0;
+    } else {
+      return 1 + Math.max(getRootHeight(tree.getLeft()), getRootHeight(tree.getRight()));
+    }
+  }
 }
