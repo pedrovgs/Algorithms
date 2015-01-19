9f5d2757c1447fb543b77d3403b5e697c2173a17
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-19 19:37:01 +0100

    Add statement to problem 65

---
+++ src/main/java/com/github/pedrovgs/problem65/IsTreeBalanced.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem65;
+
+/**
+ * Implement a function to check if a binary tree is balanced. For the purposes of this question, a
+ * balanced tree is defined to be a tree such that the heights of the two subtrees of any node
+ * never differ by more than one.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class IsTreeBalanced {
+  
+}
