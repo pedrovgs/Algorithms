18b88764aac6171dff74e54fc139997e7a111d8e
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-23 00:17:41 +0100

    Add statement to problem 68

---
+++ src/main/java/com/github/pedrovgs/problem68/IsEven.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem68;
+
+/**
+ * Given a positive int passed as parameter, can you write a method to return true if the number is
+ * even or false if the number is odd? The only constraint you have is: you can't use this
+ * operator: %.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class IsEven {
+
+}
