fffcd6d89bf17d6ec829859f4e5703c5294dd1a2
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-25 00:05:38 +0100

    Add solution to problem 70

--- src/main/java/com/github/pedrovgs/problem70/ReverseOrderOfBinaryNumber.java
+++ src/main/java/com/github/pedrovgs/problem70/ReverseOrderOfBinaryNumber.java
@@ -31,4 +31,20 @@ package com.github.pedrovgs.problem70;
  */
 public class ReverseOrderOfBinaryNumber {
 
+  /**
+   * Iterative algorithm to solve this problem. Shifting the input number until the value is equals
+   * to zero and applying a simple mask we are going to create the reverted order binary number
+   * using a constant complexity time in space terms O(1) and a linear complexity order in time
+   * terms O(N) where N is equals to the number of digits of the input parameter into a binary
+   * representation.
+   */
+  public int reverse(int number) {
+    int result = 0;
+    while (number != 0) {
+      result <<= 1;
+      result |= number & 1;
+      number >>= 1;
+    }
+    return result;
+  }
 }
