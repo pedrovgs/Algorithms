d7f0b9c1e462231ec7f97de3b5b773113f7650ff
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-25 00:04:33 +0100

    Add statement to problem 70

---
+++ src/main/java/com/github/pedrovgs/problem70/ReverseOrderOfBinaryNumber.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pedrovgs.problem70;
+
+/**
+ * Given an integer passed as parameter, can you write a method to return the number equivalent to
+ * the binary number with the reversed order?
+ *
+ * For example:
+ *
+ * Input = 5 = 101
+ * Output = 5 = 101
+ *
+ * Input = 13 = 1101
+ * Output = 11 = 1011
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class ReverseOrderOfBinaryNumber {
+
+}
