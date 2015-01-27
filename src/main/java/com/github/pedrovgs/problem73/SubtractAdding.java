a2112fe11715e1fcaef9547fd12ed6c86a83cbba
pedrovgs <pedrovicente.gomez@gmail.com>
2015-01-28 00:05:00 +0100

    Add solution to problem 73

--- src/main/java/com/github/pedrovgs/problem73/SubtractAdding.java
+++ src/main/java/com/github/pedrovgs/problem73/SubtractAdding.java
@@ -23,4 +23,21 @@ package com.github.pedrovgs.problem73;
  */
 public class SubtractAdding {
 
+  /**
+   * Iterative algorithm to resolve this problem. This solution is based on the subtraction
+   * definition where the subtraction is defined as the sum of the second number negated.
+   */
+  public int subtract(int inputA, int inputB) {
+    return inputA + negate(inputB);
+  }
+
+  private int negate(int number) {
+    int increment = number > 0 ? -1 : 1;
+    int result = 0;
+    while (number != 0) {
+      result += increment;
+      number += increment;
+    }
+    return result;
+  }
 }
