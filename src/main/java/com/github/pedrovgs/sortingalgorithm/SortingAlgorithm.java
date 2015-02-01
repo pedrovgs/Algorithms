b5291a053a31bf77e68517da0cc9c2bee197c7cb
pedrovgs <pedrovicente.gomez@gmail.com>
2015-02-01 12:14:06 +0100

    Change SortingAlgorithm to be an abstract class instead of an interface

--- src/main/java/com/github/pedrovgs/sortingalgorithm/SortingAlgorithm.java
+++ src/main/java/com/github/pedrovgs/sortingalgorithm/SortingAlgorithm.java
@@ -20,7 +20,19 @@ package com.github.pedrovgs.sortingalgorithm;
  *
  * @author Pedro Vicente Gómez Sánchez.
  */
-public interface SortingAlgorithm {
+public abstract class SortingAlgorithm {
 
-  void sort(int[] numbers);
+  public abstract void sort(int[] numbers);
+
+  protected void swap(int[] numbers, int i, int j) {
+    int temp = numbers[i];
+    numbers[i] = numbers[j];
+    numbers[j] = temp;
+  }
+
+  protected void validateInput(int[] numbers) {
+    if (numbers == null) {
+      throw new IllegalArgumentException("You can't pass a null instance as parameter.");
+    }
+  }
 }
