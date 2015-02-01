11b93bb673142f6e053627359cd59720a515f148
pedrovgs <pedrovicente.gomez@gmail.com>
2015-02-01 12:15:39 +0100

    Add statement to problem 75

---
+++ src/main/java/com/github/pedrovgs/problem75/SelectionSort.java
@@ -0,0 +1,16 @@
+package com.github.pedrovgs.problem75;
+
+import com.github.pedrovgs.sortingalgorithm.SortingAlgorithm;
+
+/**
+ * Given an array full of integers implement a selection sort algorithm to sort the content inside
+ * the array.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class SelectionSort extends SortingAlgorithm {
+
+  @Override public void sort(int[] numbers) {
+
+  }
+}
