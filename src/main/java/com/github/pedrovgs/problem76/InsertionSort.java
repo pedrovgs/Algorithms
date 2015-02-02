d5fc249f555f4ccb7956a0b6eedb8c770435e7dc
pedrovgs <pedrovicente.gomez@gmail.com>
2015-02-02 19:19:24 +0000

    Add solution to problem 76

--- src/main/java/com/github/pedrovgs/problem76/InsertionSort.java
+++ src/main/java/com/github/pedrovgs/problem76/InsertionSort.java
@@ -25,4 +25,25 @@ import com.github.pedrovgs.sortingalgorithm.SortingAlgorithm;
  */
 public class InsertionSort extends SortingAlgorithm {
 
+  /**
+   * Insertion sort is a simple sorting algorithm that is relatively efficient for small lists and
+   * mostly sorted lists, and often is used as part of more sophisticated algorithms. It works by
+   * taking elements from the list one by one and inserting them in their correct position into a
+   * new sorted list. In arrays, the new list and the remaining elements can share the array's
+   * space, but insertion is expensive, requiring shifting all following elements over by one.
+   */
+  @Override public void sort(int[] numbers) {
+    validateInput(numbers);
+
+    int i;
+    for (i = 1; i < numbers.length; i++) {
+      int tmp = numbers[i];
+      int j = i;
+      while (j > 0 && numbers[j - 1] > tmp) {
+        numbers[j] = numbers[j - 1];
+        j--;
+      }
+      numbers[j] = tmp;
+    }
+  }
 }
