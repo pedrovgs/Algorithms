f520d2cde4f3089610b1ad9eee422a54b16e662c
Woody <bigscorpions@gmail.com>
2015-12-17 00:07:51 +0800

    Use swap function to swap element in array.

--- src/main/java/com/github/pedrovgs/problem74/BubbleSort.java
+++ src/main/java/com/github/pedrovgs/problem74/BubbleSort.java
@@ -49,13 +49,12 @@ public class BubbleSort extends SortingAlgorithm {
       for (int i = 0; i < length - 1; i++) {
         for (int j = 0; j < length -i - 1; j++) {
           if (numbers[j] > numbers[j + 1]) {
-	        int tmp = numbers[j + 1];
-	        numbers[j + 1] = numbers[j];
-	        numbers[j] = tmp;
+	        swap(numbers, j, j+1);
 	        swap = true;
           }
         }
       }
     }
   }
+  
 }
