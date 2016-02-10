2c4e69eb243a81ba05d24456b3cad7e8a696f630
ajinkyakolhe112 <ajinkyakolhe112@gmail.com>
2016-02-10 15:42:28 +0530

    Using partition around pivot in quicksort for splitting arrays. Cleaner & Familiar code

--- src/test/java/com/github/pedrovgs/problem8/SplitArrayTest.java
+++ src/test/java/com/github/pedrovgs/problem8/SplitArrayTest.java
@@ -120,6 +120,52 @@ public class SplitArrayTest {
 
     assertNegativeElementsAreBeforePositiveOnes(array);
   }
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotAcceptNullArraysWithPartition() {
+        splitArray.splitSwappingPartition(null);
+    }
+
+    @Test public void shouldSupportEmptyArraysWithPartition() {
+        int[] array = new int[0];
+
+        splitArray.splitSwappingPartition(array);
+
+        assertNegativeElementsAreBeforePositiveOnes(array);
+    }
+
+    @Test public void shouldWorkWithAnArrayFullOfPositiveNumbersWithPartition() {
+        int[] array = { 1, 2, 1, 3, 4, 6 };
+
+        splitArray.splitSwappingPartition(array);
+
+        assertNegativeElementsAreBeforePositiveOnes(array);
+    }
+
+    @Test public void shouldWorkWithAnArrayFullOfNegativeNumbersWithPartition() {
+        int[] array = { 1, 2, 1, 3, 4, 6 };
+
+        splitArray.splitSwappingPartition(array);
+
+        assertNegativeElementsAreBeforePositiveOnes(array);
+    }
+
+    @Test public void shouldMoveNegativeElementsToTheLeftWithPartition() {
+        int[] array = { 1, 2, -1, -3, 4, -6 };
+
+        splitArray.splitSwappingPartition(array);
+
+        assertNegativeElementsAreBeforePositiveOnes(array);
+    }
+
+    @Test
+    public void shouldMoveNegativeElementsToTheLeftWithWithMoreNegativeElementsWithPartition() {
+        int[] array = { 1, -2, -1, -3, 4, -6 };
+
+        splitArray.splitSwappingPartition(array);
+
+        assertNegativeElementsAreBeforePositiveOnes(array);
+    }
+
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldNotAcceptNullArraysWithTwoPointersRecursive() {
