19643e391b58bca1528b9997f48ff026e2a53da4
O型血橙 <zephyrjung@126.com>
2017-02-18 17:26:45 +0800

    a little mistake
    not a big deal

--- src/main/java/com/github/pedrovgs/problem8/SplitArray.java
+++ src/main/java/com/github/pedrovgs/problem8/SplitArray.java
@@ -20,7 +20,7 @@ package com.github.pedrovgs.problem8;
  * to the left and every positive number to the right. Take into account that the order of these
  * elements among the same group(positive or negative) into the array doesn't care.
  *
- * Input: [1,2,3,-1-,2,-3] Output: [-2,-1,-3,2,3,1]
+ * Input: [1,2,3,-1,-2,-3] Output: [-2,-1,-3,2,3,1]
  *
  * @author Pedro Vicente Gómez Sánchez.
  */
