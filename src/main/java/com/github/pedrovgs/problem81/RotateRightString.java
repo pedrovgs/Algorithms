5f0cd4812d0e808606b886b33ab87924be0aa57f
rickben <ricky.benkovich@duda.co>
2020-11-03 18:54:51 +0200

    added rotate right string

--- src/main/java/com/github/pedrovgs/problem81/RotateRightString.java
+++ src/main/java/com/github/pedrovgs/problem81/RotateRightString.java
@@ -2,12 +2,12 @@ package com.github.pedrovgs.problem81;
 
 import java.util.Arrays;
 
-public class RotateStringLeft {
+public class RotateRightString {
     public static String rotateLeft(String str, int d){
         char[] result = new char[str.length()];
         char[] data = str.toCharArray();
         for (int i = 0; i < data.length; i++)
-            result[(i+d) % data.length ] = data[i];
+            result[(i+(data.length-d)) % data.length ] = data[i];
         return Arrays.toString(result);
     }
 }
