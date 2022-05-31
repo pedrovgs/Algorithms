a201daf21a4a1a37a9829c7d609d89073eb25ad8
iMichael02 <73227939+iMichael02@users.noreply.github.com>
2022-05-31 21:39:39 +0700

    Check partition method

---
+++ src/test/java/com/github/pedrovgs/problem80/PartitionTest.java
@@ -0,0 +1,19 @@
+package com.github.pedrovgs.problem80;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.pedrovgs.sortingalgorithm.SortingAlgorithm;
+import com.github.pedrovgs.sortingalgorithms.SortingAlgorithmTest;
+
+public class PartitionTest {
+
+	@Test
+	public void testPartition() {
+		int[] arr = {2,4,7,3,9,1,8};
+		QuickSort quickSortObject = new QuickSort();
+		assertTrue("partition incorrect", 5 == quickSortObject.partition(arr, 0, 6));
+	}
+
+}
