776c1c1fbc370401acce2c5af4430623012acd50
phamhuythiep12 <phamhuythiep14@gmail.com>
2022-06-01 22:07:05 +0700

    T

--- src/test/java/com/github/pedrovgs/problem20/MultiplicationWithoutMultiplyTest.java
+++ src/test/java/com/github/pedrovgs/problem20/MultiplicationWithoutMultiplyTest.java
@@ -34,16 +34,20 @@ public class MultiplicationWithoutMultiplyTest {
   @Test public void anyNumberMultipliedByZeroIsEqualsToZero() {
     assertEquals(0, multiply.calculate(0, 100));
   }
-
-  @Test public void anyNumberMultipliedByOneShouldReturnTheSameNumber() {
-    assertEquals(7, multiply.calculate(1, 7));
-  }
-
   @Test public void fiveTimesElevenEqualsToFiftyFive() {
     assertEquals(55, multiply.calculate(5, 11));
   }
 
+  @Test public void anyNumberMultipliedByOneShouldReturnTheSameNumber() {
+    assertEquals(10, multiply.calculate(1, 10));
+  }
+  @Test public void anyNumberMultipliedByTwoShouldReturnTheSameNumber() {
+    assertEquals(20, multiply.calculate(2, 10));
+  }
   @Test public void shouldTakeIntoAccountNegativeNumbers() {
-    assertEquals(-14, multiply.calculate(-2, 7));
+    assertEquals(-15, multiply.calculate(-2, 15));
+  }
+  @Test public void shouldTakeIntoAccountNegativeNumbers2() {
+    assertEquals(-20, multiply.calculate(2, -15));
   }
 }
