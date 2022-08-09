f4d4c5e112e60a13a2659c0c1f3d60085e5b43e4
LostOfStack <87476360+LostOfStack@users.noreply.github.com>
2022-08-10 06:43:44 +0800

    simple read image function

---
+++ src/main/java/com/github/pedrovgs/Problem82/ReadImage.java
@@ -0,0 +1,18 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ReadImage {
+	//create static function with name of the file as parameter
+	public static void readImage(String fileName) {
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File(fileName));
+		} catch (IOException e) {
+		}
+	}
+	
+
+}
